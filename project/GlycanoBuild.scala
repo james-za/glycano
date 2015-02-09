09ea19504e16349f33180ed79c81585e6c021dcd
James Watson <James@J>
2015-02-09 11:07:43 +0200

    upgrade scalajs and libs to 0.6.0

--- project/GlycanoBuild.scala
+++ project/GlycanoBuild.scala
@@ -3,20 +3,17 @@ import sbt.Keys._
 import sbt._
 
 import org.scalajs.sbtplugin.ScalaJSPlugin
-import org.scalajs.sbtplugin.ScalaJSPlugin._
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 
 import com.lihaoyi.workbench.Plugin._
 
-object GlycanoBuild extends Build {
-  val glycanoScalaVersion = "2.11.5"
-
+object GlycanoBuild extends sbt.Build {
   lazy val root = project.in(file("."))
     .enablePlugins(ScalaJSPlugin)
     .settings(workbenchSettings: _*)
     .settings(
       name := "GlycanoWeb",
-      scalaVersion := glycanoScalaVersion,
+      scalaVersion := "2.11.5",
       scalacOptions ++= Seq(
         "-deprecation",
         "-unchecked",
@@ -27,29 +24,27 @@ object GlycanoBuild extends Build {
       resolvers ++= Seq(
         Resolver.sonatypeRepo("snapshots"),
         Resolver.sonatypeRepo("releases"),
-        Resolver.defaultLocal,
         Resolver.url("sjs-release", url("http://dl.bintray.com/scala-js/scala-js-releases"))(Resolver.ivyStylePatterns),
         "myltsev" at "http://dl.bintray.com/alexander-myltsev/maven"
       ),
       libraryDependencies ++= Seq(
         "org.scala-js"                      %%% "scalajs-dom"       % "0.7.0",
         "be.doeraene"                       %%% "scalajs-jquery"    % "0.7.0",
-        "com.lihaoyi"                       %%% "scalatags"         % "0.4.3-RC1",
-        "com.lihaoyi"                       %%% "scalarx"           % "0.2.7-RC1",
-        "com.lihaoyi"                       %%% "upickle"           % "0.2.6-RC1",
-        "com.github.japgolly.fork.scalaz"   %%% "scalaz-core"       % "7.1.0-4",
-        "com.github.japgolly.scalajs-react" %%% "core"              % "0.7.2-RC1",
-        "com.github.japgolly.scalajs-react" %%% "ext-monocle"       % "0.7.2-RC1",
-        "com.github.japgolly.scalajs-react" %%% "ext-scalaz71"      % "0.7.2-RC1",
+        "com.lihaoyi"                       %%% "scalatags"         % "0.4.5",
+        "com.lihaoyi"                       %%% "scalarx"           % "0.2.7",
+        "com.lihaoyi"                       %%% "upickle"           % "0.2.6",
+        "com.github.japgolly.fork.scalaz"   %%% "scalaz-core"       % "7.1.1",
+        "com.github.japgolly.scalajs-react" %%% "core"              % "0.7.2",
+        "com.github.japgolly.scalajs-react" %%% "ext-monocle"       % "0.7.2",
+        "com.github.japgolly.scalajs-react" %%% "ext-scalaz71"      % "0.7.2",
         "com.github.japgolly.fork.monocle"  %%% "monocle-core"      % "1.0.1",
         "com.github.japgolly.fork.monocle"  %%% "monocle-macro"     % "1.0.1",
         "name.myltsev"                      %%% "shapeless"         % "2.0.0",
-        "org.parboiled"                     %%% "parboiled"         % "2.0.1_3"
+        "org.parboiled"                     %%% "parboiled"         % "2.0.1"
       ),
       bootSnippet := "za.jwatson.glycanoweb.GlycanoWeb().main();",
       refreshBrowsers <<= refreshBrowsers.triggeredBy(fastOptJS in Compile),
       relativeSourceMaps := true,
-      emitSourceMaps := false,
       addCompilerPlugin(compilerPlugin("org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full))
     )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.0-RC1")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.0")
 
 resolvers += "spray repo" at "http://repo.spray.io"
 
